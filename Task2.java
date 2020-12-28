788a058c197f3b9fb2aaac2d9daa750cfffaa591
Alexander <1990klik@mail.ru>
2020-12-28 18:51:10 +0300

    commit for branch

---
+++ Task2.java
@@ -0,0 +1,55 @@
+// import java.util.Scanner;
+
+// public class Task2 {
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+// double X = input.nextDouble();
+// boolean Y = X + 4 % 3 - X + (11 - 2 / 2 * 0 - (X * -1)) >= X;
+// boolean Z = !(X <= 3 && Y == false) || X >= 3;
+// System.out.print(Z);
+
+// double one = input.nextDouble();
+// double two = input.nextDouble();
+// double three = input.nextDouble();
+// double four = input.nextDouble();
+// double five = input.nextDouble();
+// double multip = one * two * three * four * five;
+// double addition = one + two + three + four + five;
+// boolean result = multip >= addition;
+// System.out.println(result);
+// double one = input.nextDouble();
+// double two = input.nextDouble();
+// double three = input.nextDouble();
+// double four = input.nextDouble();
+// double five = input.nextDouble();
+// double res = one + two + three + four + five;
+// System.out.println("Your summ = " + res);
+
+// String str = input.next();
+// System.out.println("You Just entered " + str);
+// double one;
+// double two;
+// double three;
+// one = input.nextDouble();
+// two = one * 5;
+// System.out.println(two);
+// three = one + two;
+// System.out.println(three);
+// one = one + three;
+// System.out.println(one);
+// double delimoe = input.nextDouble();
+// double delitel = input.nextDouble();
+// double resultDelenie = delimoe % delitel;
+// System.out.println(resultDelenie);
+
+// Scanner input = new Scanner(System.in);
+// double num = input.nextDouble();
+// boolean result = num < 100 && num > 20;
+// System.out.println(result);
+
+// double num1 = input.nextDouble();
+// double num2 = input.nextDouble();
+// double num3 = num1 * num1 + num2 * num2;
+// System.out.println(num3);
+// }
+// }
