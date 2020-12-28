f058bafe0a0c45698f21e3f275768ae7f33843a0
Alexander <1990klik@mail.ru>
2020-12-28 18:38:26 +0300

    second commit

---
+++ Task3.java
@@ -0,0 +1,183 @@
+import java.util.Scanner;
+
+public class Task3 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		// String first = input.next();
+		// String second = input.next();
+		// String third = input.next();
+		// String fourth = input.next();
+		// String fifth = input.next();
+		// System.out.print(first + " " + second + " " + third + " " + fourth + " " +
+		// fifth);
+
+		// double a = input.nextDouble();
+		// double b = input.nextDouble();
+		// double c = input.nextDouble();
+		// double d = input.nextDouble();
+		// double e = input.nextDouble();
+		// double sum = a + b + c + d + e;
+		// double sumOstatok = sum % a;
+		// double aPopolam = a / 2;
+		// System.out.println(sumOstatok);
+		// boolean result = 0 < sumOstatok && sumOstatok < aPopolam;
+		// System.out.println(result);
+
+		// String word = input.next();
+		// double first = input.nextDouble();
+		// double second = input.nextDouble();
+		// double result = first + second;
+		// System.out.println(first + " " + word + " + " + second + " " + word + " = " +
+		// result + " " + word);
+
+		// double a = input.nextDouble();
+		// double b = input.nextDouble();
+		// double c = input.nextDouble();
+		// double sumAb = a + b;
+		// double sumBc = b + c;
+		// double kvadratC = c * c;
+		// boolean bool = a > sumBc || kvadratC > sumAb;
+		// System.out.print(bool);
+
+		// double q = input.nextDouble();
+		// double w = input.nextDouble();
+		// double e = input.nextDouble();
+		// double r = input.nextDouble();
+		// double t = input.nextDouble();
+		// double y = input.nextDouble();
+		// double sumRTY = r + t + y;
+		// double ostatokRTY = sumRTY % w;
+		// double raznicaQT = q - t;
+		// double squareW = w * w;
+		// double squareR = r * r;
+		// double sumSquareWR = squareW + squareR;
+		// double squareQ = q * q;
+		// double squareY = y * y;
+		// double sumSquareQY = squareQ + squareY;
+		// boolean result = ostatokRTY > raznicaQT || sumSquareWR > sumSquareQY || e ==
+		// 10;
+		// System.out.print(result);
+
+		// double one = input.nextDouble();
+		// double two = input.nextDouble();
+		// double three = input.nextDouble();
+		// double four = input.nextDouble();
+		// double five = input.nextDouble();
+		// double six = input.nextDouble();
+		// double seven = input.nextDouble();
+		// double eight = input.nextDouble();
+		// double nine = input.nextDouble();
+		// double ten = input.nextDouble();
+		// double sumFirst5 = one + two + three + four + five;
+		// double squareNine = nine * nine;
+		// double squareTen = ten * ten;
+		// double nineМinusTen = squareTen - squareNine;
+		// double sumAll = one + two + three + four + five + six + seven + eight + nine
+		// + ten;
+		// boolean result = sumFirst5 <= nineМinusTen || sumAll <= 100;
+		// System.out.print(result);
+
+		// double one = input.nextDouble();
+		// double two = input.nextDouble();
+		// double three = input.nextDouble();
+		// double four = input.nextDouble();
+		// double five = input.nextDouble();
+		// double six = input.nextDouble();
+		// boolean result = one > 3 && two > one && three > two && four > three && five
+		// > four && six > five;
+		// System.out.print(result);
+
+		// double one = input.nextDouble();
+		// double two = input.nextDouble();
+		// double three = input.nextDouble();
+		// double four = input.nextDouble();
+		// double five = input.nextDouble();
+		// double six = input.nextDouble();
+		// double one50 = one * 0.5 + one;
+		// double two50 = two * 0.5 + two;
+		// double three50 = three * 0.5 + three;
+		// double four50 = four * 0.5 + four;
+		// double five50 = five * 0.5 + five;
+		// double six50 = six * 0.5 + six;
+		// double one2 = one * 2;
+		// boolean result = two >= one50 && three >= two50 && four >= three50 && five >=
+		// four50 && six >= five50
+		// && one2 <= six;
+		// System.out.print(result);
+
+		// double a = input.nextDouble();
+		// double b = input.nextDouble();
+		// double c = input.nextDouble();
+		// double d = input.nextDouble();
+		// double e = input.nextDouble();
+		// double f = input.nextDouble();
+		// double aa = a;
+		// double bb = b;
+		// double cc = c;
+		// double dd = d;
+		// double ee = e;
+		// double ff = f;
+		// f = aa;
+		// a = ff;
+		// b = ee;
+		// e = bb;
+		// c = dd;
+		// d = cc;
+		// System.out.print(a);
+		// System.out.print(b);
+		// System.out.print(c);
+		// System.out.print(d);
+		// System.out.print(e);
+		// System.out.print(f);
+
+		// double a = input.nextDouble();
+		// double b = input.nextDouble();
+		// // a = a + b - (b = a);
+		// a = a + b;
+		// b = a - b;
+		// a = a - b;
+		// System.out.println(a);
+		// System.out.println(b);
+
+		// double metr = input.nextDouble();
+		// double result = metr / 1000;
+		// System.out.println(result);
+
+		// double cels = input.nextDouble();
+		// double far = cels * 1.8 + 32;
+		// double kel = cels + 273.15;
+		// System.out.println(far);
+		// System.out.println(kel);
+
+		// double first = input.nextDouble();
+		// double second = input.nextDouble();
+		// double third = input.nextDouble();
+		// double fS = first + second;
+		// double fT = first + third;
+		// double tS = second + third;
+		// boolean result = fS >= third && fT >= second && tS >= first;
+		// System.out.println(result);
+
+		// double a = input.nextDouble();
+		// double b = input.nextDouble();
+		// double c = input.nextDouble();
+		// double abc = a + b + c;
+		// boolean result = abc >= 180;
+		// System.out.println(result);
+
+		// double d = input.nextDouble();
+		// double km = input.nextDouble();
+		// double DD = d / 39370;
+		// boolean result = DD >= km;
+		// System.out.println(result);
+
+		// double a = input.nextDouble();
+		// double b = input.nextDouble();
+		// double c = input.nextDouble();
+		// boolean aa = a != 0;
+		// System.out.println(aa);
+		// double DD = b * b - 4 * a * c;
+		// boolean result = DD > 0;
+		// System.out.println(result);
+	}
+}
